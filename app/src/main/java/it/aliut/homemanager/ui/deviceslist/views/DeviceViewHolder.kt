33ebc3959b07418ae91b953b68c9fffe6cd73db7
a-liut <andrea.liut@gmail.com>
2020-01-14 17:33:21 +0100

    feat: cards in devices list.

--- app/src/main/java/it/aliut/homemanager/ui/deviceslist/views/DeviceViewHolder.kt
+++ app/src/main/java/it/aliut/homemanager/ui/deviceslist/views/DeviceViewHolder.kt
@@ -2,6 +2,7 @@ package it.aliut.homemanager.ui.deviceslist.views
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import it.aliut.homemanager.R
 import it.aliut.homemanager.model.Device
 import it.aliut.homemanager.ui.deviceslist.DeviceAdapter
 import kotlinx.android.synthetic.main.item_deviceitem.view.*
@@ -16,7 +17,8 @@ class DeviceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             with(itemView) {
                 textview_deviceitem_name.text = device.name
                 textview_deviceitem_address.text = device.address
-                textview_deviceitem_lastupdate.text = device.updatedAt
+                textview_deviceitem_lastupdate.text =
+                    context.getString(R.string.deviceitem_lastupdate, device.updatedAt)
 
                 setOnClickListener {
                     clickListener.onDeviceClicked(device)
